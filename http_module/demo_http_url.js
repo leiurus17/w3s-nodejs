5f7fe6cfea9c1964c20b690ffd524c2baada019d
Marlon <martianworm17@gmail.com>
2018-02-03 18:16:48 +0800

    Read the Query String

---
+++ http_module/demo_http_url.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+// Create a server object
+http.createServer(function (req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write('Hello World and Hello Konoha!'); // Write a response to the client
+	res.end(); // End the response
+}).listen(8080); // The server object listens on port 8080
